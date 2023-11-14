816067114a62102e37cb239991ffdf2d4d080197
JoãoVicentim <joaopedrosantosvicentim@gmail.com>
2023-11-13 21:03:40 -0300

    13.11

---
+++ contas/contaCorrente.go
@@ -0,0 +1,44 @@
+package contas
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+// Recebe como parâmetro o valorDoSaque
+// Deve retornar uma string
+// c * ContaCorrente -> Isso faz com que a função aponte para a conta que esta querendo realizar o saque
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	if podeSacar {
+		c.Saldo -= valorDoSaque
+		return "Saque realizado com sucesso!\nNovo Saldo disponível:", c.Saldo
+	} else {
+		return "Sem Saldo disponível!\nSaldo continua com:", c.Saldo
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	podeDepositar := valorDoDeposito > 0
+	if podeDepositar {
+		c.Saldo += valorDoDeposito
+		return "Depósito realizado. Valor depositado:", valorDoDeposito
+	} else {
+		return "Depósito não realizado", 0
+	}
+
+}
+
+func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
+
+	if valorTransferencia < c.Saldo && valorTransferencia > 0 {
+		c.Saldo -= valorTransferencia
+
+		contaDestino.Depositar(valorTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-//Recebe como parâmetro o valorDoSaque
-//Deve retornar uma string
-//c * ContaCorrente -> Isso faz com que a função aponte para a conta que esta querendo realizar o saque
-func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso!\nNovo saldo disponível:", c.saldo
-	} else {
-		return "Sem saldo disponível!\nSaldo continua com:", c.saldo
-	}
-}
+	"Alura-Poo-Go/contas"
+)
 
 func main() {
 	// contaDoJoao := ContaCorrente{titular: "Joao",
@@ -39,12 +23,19 @@ func main() {
 
 	// fmt.Println(*contaDaGigi)
 
-	contadaSilvia := ContaCorrente{}
+	contaMarcela := contas.ContaCorrente{}
+	contaMarcela.titular = "Marcela"
+	contaMarcela.saldo = 1000
+
+	contadaSilvia := contas.ContaCorrente{}
 	contadaSilvia.titular = "Silvia"
 	contadaSilvia.saldo = 500
 
-	fmt.Println(contadaSilvia)
+	status := contaMarcela.Transferir(100, &contadaSilvia)
+	fmt.Println(status)
 
-	fmt.Println(contadaSilvia.Sacar(1000))
-	fmt.Println("spinola")
+	fmt.Println(contadaSilvia)
+	fmt.Println(contaMarcela)
+
+	//fmt.Println(contadaSilvia.Sacar(1000))
 }
